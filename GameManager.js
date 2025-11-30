8db8acdc5d7ae8737f020557461931ee4591d0f1
Michael <crazedmonkey231@users.noreply.github.com>
2025-11-30 18:44:24 -0500

    converted to thing system

--- GameManager.js
+++ GameManager.js
@@ -32,9 +32,9 @@ class GameManager {
     app.post("/api/gameManager/playerNotify", this.playerNotify.bind(this));
 
     // Expose endpoint to get number of players in a game
-    app.get("/api/gameManager/playersInGame/:gameId", (req, res) => {
-      const { gameId } = req.params;
-      const game = this.getGameState(gameId);
+    app.get("/api/gameManager/playersInGame/:gameId/:roomId", (req, res) => {
+      const { gameId, roomId } = req.params;
+      const game = this.getGameState(gameId, roomId);
       res.json({ playerCount: Object.keys(game.players).length });
     });
 
@@ -72,50 +72,41 @@ class GameManager {
       socket.data.gameId = gameId; // store on socket
       socket.data.roomId = roomId; // store on socket
 
-      const roomName = getRoomName(socket);
+      let game = this.getGameStateFromSocket(socket);
+      let roomName = game.roomName;
+
       socket.join(roomName); // join that game’s room
 
       console.log(
         `Socket ${socket.id} joined game ${gameId}, room ${roomName}`
       );
 
-      const game = this.getGameState(gameId);
-      console.log(
-        `Current players in game ${gameId}:`,
-        Object.keys(game.players)
-      );
-
       // Add player to game state
-      game.players[socket.id] = {
+      const player = {
         roomId,
+        id: socket.id,
         name: name || `${socket.id}`,
         score: parseInt(score) || 0,
         speed: parseFloat(speed) || 0.01,
+        type: "BasicBoxThing",
         transform: JSON.parse(transform) || {
           position: { x: 0, y: 0, z: 0 },
           rotation: { isEuler: true, _x: 0, _y: 0, _z: 0, _order: "XYZ" },
           scale: { x: 1, y: 1, z: 1 },
         },
       };
+      game.things[socket.id] = player;
 
       // Send them initial state for THIS game
       socket.emit("init", {
         you: socket.id,
-        players: game.players,
         things: game.things,
-      });
-
-      // Let other players in *this game* know
-      socket.to(roomName).emit("playerJoined", {
-        id: socket.id,
-        ...game.players[socket.id],
       });
 
       // Handle input for THIS game only
       socket.on("playerInput", (input) => {
         const player = this.getPlayer(socket);
         if (!player) return;
-        if (!input) return;
         if (gameId === "default-game") {
           const speed = player.speed;
           const playerPos = player.transform.position;
@@ -126,8 +117,7 @@ class GameManager {
           if (input.up) playerPos.y += speed;
           if (input.down) playerPos.y -= speed;
           // Broadcast new position to room
-          const roomName = getRoomName(socket);
-          io.to(roomName).emit("playerMoved", {
+          io.to(roomName).emit("thingMoved", {
             id: socket.id,
             position: player.transform.position,
           });
@@ -138,204 +128,62 @@ class GameManager {
         const player = this.getPlayer(socket);
         if (!player) return;
         player.score += points;
-        const roomName = getRoomName(socket);
         io.to(roomName).emit("playerScored", {
           id: socket.id,
           score: player.score,
         });
       });
 
-      socket.on("playerTransform", (transform) => {
-        const player = this.getPlayer(socket);
-        if (!player) return;
-        player.transform = transform;
-        const roomName = getRoomName(socket);
-        io.to(roomName).emit("playerTransformed", {
-          id: socket.id,
-          transform: player.transform,
-        });
-      });
-
-      socket.on("playerPosition", (position) => {
-        const player = this.getPlayer(socket);
-        if (!player) return;
-        player.transform.position = position;
-        const roomName = getRoomName(socket);
-        io.to(roomName).emit("playerMoved", {
-          id: socket.id,
-          position: player.transform.position,
-        });
-      });
-
-      socket.on("playerRotation", (rotation) => {
-        const player = this.getPlayer(socket);
-        if (!player) return;
-        player.transform.rotation = rotation;
-        const roomName = getRoomName(socket);
-        io.to(roomName).emit("playerRotated", {
-          id: socket.id,
-          rotation: player.transform.rotation,
-        });
-      });
-
-      socket.on("playerScale", (scale) => {
-        const player = this.getPlayer(socket);
-        if (!player) return;
-        player.transform.scale = scale;
-        const roomName = getRoomName(socket);
-        io.to(roomName).emit("playerScaled", {
-          id: socket.id,
-          scale: player.transform.scale,
-        });
-      });
-
-      socket.on("playerPositionRotation", ({ position, rotation }) => {
-        const player = this.getPlayer(socket);
-        if (!player) return;
-        player.transform.position = position;
-        player.transform.rotation = rotation;
-        const roomName = getRoomName(socket);
-        io.to(roomName).emit("playerMovedRotated", {
-          id: socket.id,
-          position: player.transform.position,
-          rotation: player.transform.rotation,
-        });
-      });
-
-      socket.on("playerPositionScale", ({ position, scale }) => {
-        const player = this.getPlayer(socket);
-        if (!player) return;
-        player.transform.position = position;
-        player.transform.scale = scale;
-        const roomName = getRoomName(socket);
-        io.to(roomName).emit("playerMovedScaled", {
-          id: socket.id,
-          position: player.transform.position,
-          scale: player.transform.scale,
-        });
-      });
-
-      socket.on(
-        "playerPositionRotationScale",
-        ({ position, rotation, scale }) => {
-          const player = this.getPlayer(socket);
-          if (!player) return;
-          player.transform.position = position;
-          player.transform.rotation = rotation;
-          player.transform.scale = scale;
-          const roomName = getRoomName(socket);
-          io.to(roomName).emit("playerMovedRotatedScaled", {
-            id: socket.id,
-            position: player.transform.position,
-            rotation: player.transform.rotation,
-            scale: player.transform.scale,
-          });
-        }
-      );
-
       // -- Thing management --
 
       socket.on("spawnThing", (thingData) => {
-        const game = this.getGameState(socket.data.gameId);
         game.things[thingData.id] = thingData;
-        const roomName = getRoomName(socket);
         io.to(roomName).emit("thingSpawned", thingData);
       });
 
-      socket.on("despawnThing", (thingId) => {
-        const game = this.getGameState(socket.data.gameId);
-        delete game.things[thingId];
-        const roomName = getRoomName(socket);
-        io.to(roomName).emit("thingDespawned", thingId);
+      socket.on("addThing", (thingData) => {
+        game.things[thingData.id] = thingData;
+        io.to(roomName).emit("thingAdded", thingData);
       });
 
-      socket.on("killThing", (thingId) => {
-        const game = this.getGameState(socket.data.gameId);
+      socket.on("removeThing", (thingId) => {
         delete game.things[thingId];
-        const roomName = getRoomName(socket);
-        io.to(roomName).emit("thingKilled", thingId);
-      });
-
-      socket.on("disposeThing", (thingId) => {
-        const game = this.getGameState(socket.data.gameId);
-        delete game.things[thingId];
-        const roomName = getRoomName(socket);
-        io.to(roomName).emit("thingDisposed", thingId);
+        io.to(roomName).emit("thingRemoved", thingId);
       });
 
       socket.on("clearAllThings", () => {
-        const game = this.getGameState(socket.data.gameId);
         game.things = {};
-        const roomName = getRoomName(socket);
         io.to(roomName).emit("allThingsCleared");
       });
 
       socket.on("thingPosition", ({ id, position }) => {
-        const game = this.getGameState(socket.data.gameId);
-        if (!game.things[id]) return;
-        const transform = game.things[id].transform;
-        transform.position = position;
-        const roomName = getRoomName(socket);
-        io.to(roomName).emit("thingMoved", { id, position });
+        this.updateThingTransform(socket, id, { position });
       });
 
       socket.on("thingRotation", ({ id, rotation }) => {
-        const game = this.getGameState(socket.data.gameId);
-        if (!game.things[id]) return;
-        const transform = game.things[id].transform;
-        transform.rotation = rotation;
-        const roomName = getRoomName(socket);
-        io.to(roomName).emit("thingRotated", { id, rotation });
+        this.updateThingTransform(socket, id, { rotation });
       });
 
       socket.on("thingScale", ({ id, scale }) => {
-        const game = this.getGameState(socket.data.gameId);
-        if (!game.things[id]) return;
-        const transform = game.things[id].transform;
-        transform.scale = scale;
-        const roomName = getRoomName(socket);
-        io.to(roomName).emit("thingScaled", { id, scale });
+        this.updateThingTransform(socket, id, { scale });
       });
 
       socket.on("thingPositionRotation", ({ id, position, rotation }) => {
-        const game = this.getGameState(socket.data.gameId);
-        if (!game.things[id]) return;
-        const transform = game.things[id].transform;
-        transform.position = position;
-        transform.rotation = rotation;
-        const roomName = getRoomName(socket);
-        io.to(roomName).emit("thingMovedRotated", {
-          id, position, rotation
-        });
+        this.updateThingTransform(socket, id, { position, rotation });
       });
 
       socket.on("thingPositionScale", ({ id, position, scale }) => {
-        const game = this.getGameState(socket.data.gameId);
-        if (!game.things[id]) return;
-        const transform = game.things[id].transform;
-        transform.position = position;
-        transform.scale = scale;
-        const roomName = getRoomName(socket);
-        io.to(roomName).emit("thingMovedScaled", {
-          id, position, scale
-        });
+        this.updateThingTransform(socket, id, { position, scale });
       });
 
-      socket.on("thingPositionRotationScale", ({ id, position, rotation, scale }) => {
-        const game = this.getGameState(socket.data.gameId);
-        if (!game.things[id]) return;
-        const transform = game.things[id].transform;
-        transform.position = position;
-        transform.rotation = rotation;
-        transform.scale = scale;
-        const roomName = getRoomName(socket);
-        io.to(roomName).emit("thingMovedRotatedScaled", {
-          id, position, rotation, scale
-        });
-      });
+      socket.on(
+        "thingPositionRotationScale",
+        ({ id, position, rotation, scale }) => {
+          this.updateThingTransform(socket, id, { position, rotation, scale });
+        }
+      );
 
       socket.on("chatMessage", (message) => {
-        const roomName = getRoomName(socket);
         io.to(roomName).emit("chatMessage", {
           from: socket.id,
           message,
@@ -343,66 +191,125 @@ class GameManager {
       });
 
       socket.on("getAllRoomPlayerCount", () => {
-        const gameId = socket.data.gameId;
-        const game = this.getGameState(gameId);
         const roomCounts = {};
         for (const roomId of VALID_ROOM_ID) {
           roomCounts[roomId] = 0;
-        }
-        for (const socketId of Object.keys(game.players)) {
-          const player = game.players[socketId];
-          roomCounts[player.roomId] += 1;
+          for (const thing of Object.values(game.things)) {
+            if (thing.gameplayTags?.includes("player")) {
+              roomCounts[roomId] += 1;
+            }
+          }
         }
         socket.emit("allRoomPlayerCount", roomCounts);
       });
 
       socket.on("playerChangeRoom", (newRoomId) => {
         if (!VALID_ROOM_ID.includes(newRoomId)) return;
-        const gameId = socket.data.gameId;
-        const oldRoomId = socket.data.roomId;
-        const oldRoomName = `${gameId}:${ROOM_ID_PREFIX}${oldRoomId}`;
-        const newRoomName = `${gameId}:${ROOM_ID_PREFIX}${newRoomId}`;
-        socket.leave(oldRoomName);
-        socket.join(newRoomName);
+        io.to(roomName).emit("playerLeft", socket.id);
+        delete game.things[socket.id];
+        socket.leave(roomName);
+
         socket.data.roomId = newRoomId;
-        const game = this.getGameState(gameId);
-        game.players[socket.id].roomId = newRoomId;
-        io.to(oldRoomName).emit("playerLeftRoom", socket.id);
-        io.to(newRoomName).emit("playerJoinedRoom", {
-          id: socket.id,
-          ...game.players[socket.id],
+
+        game = this.getGameStateFromSocket(socket);
+        roomName = game.roomName;
+        player.roomId = newRoomId;
+        game.things[socket.id] = player;
+        socket.join(roomName);
+
+        io.to(roomName).emit("playerJoined", {
+          player: player,
+          things: game.things,
         });
       });
 
       socket.on("disconnect", () => {
-        const gameId = socket.data.gameId;
-        const roomName = getRoomName(socket);
-        const game = this.getGameState(gameId);
-        delete game.players[socket.id];
+        delete game.things[socket.id];
         io.to(roomName).emit("playerLeft", socket.id);
       });
     });
   }
 
-  getPlayer(socket) {
-    const gameId = socket.data.gameId;
-    const game = this.getGameState(gameId);
-    return game.players[socket.id];
+  /**
+   * Update a thing's transform and emit appropriate event
+   * @param {object} socket - The socket of the player making the update
+   * @param {string} thingId - The ID of the thing to update
+   * @param {object} newTransform - The new transform data (position, rotation, scale)
+   *
+   * Emits:
+   * - "thingMoved" if position is updated
+   * - "thingRotated" if rotation is updated
+   * - "thingScaled" if scale is updated
+   * - Combination events like "thingMovedRotated", etc. if multiple are updated
+   */
+  updateThingTransform(socket, thingId, newTransform) {
+    const game = this.getGameStateFromSocket(socket);
+    if (!game.things[thingId]) return;
+    const transform = game.things[thingId].transform;
+    let hasPosition = false;
+    let hasRotation = false;
+    let hasScale = false;
+
+    let emitEvent = "thing";
+    const data = {
+      id: thingId,
+    };
+
+    if (newTransform.position) {
+      hasPosition = true;
+      emitEvent += "Moved";
+      data.position = newTransform.position;
+      transform.position = newTransform.position;
+    }
+    if (newTransform.rotation) {
+      hasRotation = true;
+      emitEvent += "Rotated";
+      data.rotation = newTransform.rotation;
+      transform.rotation = newTransform.rotation;
+    }
+    if (newTransform.scale) {
+      hasScale = true;
+      emitEvent += "Scaled";
+      data.scale = newTransform.scale;
+      transform.scale = newTransform.scale;
+    }
+
+    if (!hasPosition && !hasRotation && !hasScale) return;
+    this.io.to(game.roomName).emit(emitEvent, data);
   }
 
-  getGameState(gameId) {
+  getPlayer(socket) {
+    const game = this.getGameStateFromSocket(socket);
+    return game.things[socket.id];
+  }
+
+  getThing(gameId, roomId, thingId) {
+    const game = this.getGameState(gameId, roomId);
+    return game.things[thingId];
+  }
+
+  getGameStateFromSocket(socket) {
+    const gameId = socket.data.gameId;
+    const roomId = socket.data.roomId;
+    return this.getGameState(gameId, roomId);
+  }
+
+  getGameState(gameId, roomId) {
     if (!this.gameStates[gameId]) {
-      this.gameStates[gameId] = {
-        players: {},
+      this.gameStates[gameId] = {};
+    }
+    if (!this.gameStates[gameId][roomId]) {
+      this.gameStates[gameId][roomId] = {
+        roomName: `${gameId}:${ROOM_ID_PREFIX}${roomId}`,
         things: {},
       };
     }
-    return this.gameStates[gameId];
+    return this.gameStates[gameId][roomId];
   }
 
   playerNotify(req, res) {
     const { message } = req.body;
-    console.log("Player Notify:", message);
+    console.warn("Player Notify:", message);
     this.io.emit("playerNotify", { message });
     res.json({ success: true });
   }
